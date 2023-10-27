67073159d9dcc8d1d5ee165025454f2f7e06872d
samelimill <samelimill@gmail.com>
2023-10-26 20:33:36 -0700

    Added env example and user model

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const Comment = require('./Comment')
+const Gallery = require('./Gallery')
+const Post = require('./Post');
+const Tattoo = require('./Tattoo');
+const User = require("./User");
+
+
+module.exports = { Comment, Gallery, Post, Tattoo, User };
