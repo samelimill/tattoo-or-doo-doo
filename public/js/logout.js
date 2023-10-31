2bac0ba6cb223e5aba58aaa85a371ed3c30e060e
dsatpm <jeremiahjh@gmail.com>
2023-10-30 22:00:23 -0700

    added signup form, signup handlebars

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,16 @@
-const logout = async () => {
-  const response = await fetch('/api/users/logout', {
-    method: 'POST', 
-    headers: {'Content-Type': 'application/json'},
-  });
+const { on } = require('events');
 
-  if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert('Failed to log out.');
-  }
+const logout = async () => {
+	const response = await fetch('/api/users/logout', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+	});
+
+	if (response.ok) {
+		document.location.replace('/');
+	} else {
+		alert('Failed to log out.');
+	}
 };
 
-document.querySelector('#logout').addEventListener('click', logout);
+// document.querySelector('#logout').addEventListener('click', logout);
