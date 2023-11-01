44cfd204bdb74a5c37ca2304eb48537a166038b0
Jesse Warwick <jkeller647@gmail.com>
2023-10-31 21:25:02 -0700

    made the js file to create a post and have it interact with the post route, bare bones

---
+++ public/js/createpost.js
@@ -0,0 +1,32 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Target the form by its ID
+    const form = document.getElementById("myForm");
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault(); // Prevent the default form submission behavior
+
+      // Create a FormData object to collect the form data
+      const formData = new FormData(form);
+
+      // Use the fetch API to send a POST request to the /tattoos route
+      fetch("/tattoos", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("New Tattoo Post Created:", data);
+          // You can add further actions here, like redirecting the user or displaying a success message.
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          // Handle errors here, like displaying an error message to the user.
+        });
+    });
+  });
