acce267ca0383623b9462b7b283ebe441d4ec1ea
dsatpm <jeremiahjh@gmail.com>
2023-11-02 19:35:16 -0700

    changed a whole lot of stuff

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
+  res.render('login');
+})
+
+router.get('/login', async (req, res) => {
+  res.render('login');
+});
+
+// route for signup
+router.get('/signup', async (req, res) => {
+  res.render('signup');
+});
+
+// route for user's profile
+router.get('/profile', async (req, res) => {
+  res.render('profile');
+});
+
+// route for homepage
+router.get('/homepage', (req, res) => {
   res.render('homepage');
 });
 
+router.get('/gallery', (req, res) => {
+  res.render('gallery');
+});
+
 module.exports = router;
+
